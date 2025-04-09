224dcd6ccfb4fe87385aa20bf2699787d48200dd
Gunvich <gunderovich.work@gmail.com>
2025-04-09 22:37:32 +0300

    test commit

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/about', function () { return view('about');});
 Route::get('/', function () { return view('home');});
-
-
+Route::get('/contact', function () { return view('contact');});
 Route::get('/user/{id}/{name}', function ($id, $name) { return view('about');});
