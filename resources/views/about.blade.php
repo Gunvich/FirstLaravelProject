e01b8e4c720ebe87b8a442a5dee51f55c97ffe6e
Gunvich <gunderovich.work@gmail.com>
2025-04-09 22:21:25 +0300

    repositori init + view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('title')About Us @endsection
+
+@section('main_content')
+<div class="jumbotron bg-warning">
+  <h1>About us</h1>
+  <p class="lead"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse minus qui eum ipsum, dolorum repellendus sed, debitis repudiandae reiciendis, tempore eaque vero aut nobis architecto doloremque ipsa. Error, doloribus non! </p>
+  <a class="btn btn-lg btn-danger" href="/review" role="button">Reviews</a>
+</div>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title')Main page @endsection
+ 
+@section('main_content')
+ <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur quis odit sunt numquam quod dignissimos voluptatem aut laborum nam, odio libero repellendus iusto error est placeat hic cum consequuntur nostrum?</p>
+@endsection
