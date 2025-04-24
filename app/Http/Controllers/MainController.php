4477cf7007303aaccf0c0604a3342628a440defe
Gunvich <gunderovich.work@gmail.com>
2025-04-25 02:50:40 +0300

    Taking data from the form, adding them to data base

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reviews;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -29,5 +30,13 @@ class MainController extends Controller
                 'message' => 'required|min:5|max:500',
             ]
         );
+        $review = new Reviews();
+        $review -> email =$request->input('email');
+        $review -> subject =$request->input('subject');
+        $review -> message =$request->input('message');
+
+        $review -> save();
+
+        return redirect('/review');
     }
 }
